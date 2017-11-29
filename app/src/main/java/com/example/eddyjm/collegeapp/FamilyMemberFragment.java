c2aebc7a7f575c41224fe120eed44f2d69b477b1
jeddy19 <eddyjm@westclarkschools.com>
2017-11-29 12:52:23 -0500

    commit message 3: added TextView/EditText, FamilyMember class, and wired up

--- app/src/main/java/com/example/eddyjm/collegeapp/FamilyMemberFragment.java
+++ app/src/main/java/com/example/eddyjm/collegeapp/FamilyMemberFragment.java
@@ -7,8 +7,12 @@ import android.support.v4.view.ViewGroupCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.eddyjm.collegeapp.R;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by eddyjm on 11/16/2017.
@@ -16,11 +20,21 @@ import com.example.eddyjm.collegeapp.R;
 
 public class FamilyMemberFragment extends android.support.v4.app.Fragment {
 
+    TextView mFMFirstName;
+    TextView mFMLastName;
+    EditText mFMEdit;
+    FamilyMember mFamilyMember = new FamilyMember();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_family_member, container, false);
+
+        mFMFirstName = (TextView) rootView.findViewById(R.id.FirstName);
+        mFMLastName = (TextView) rootView.findViewById(R.id.LastName);
+        mFMFirstName.setText(mFamilyMember.getFirstName());
+        mFMLastName.setText(mFamilyMember.getLastName());
 
         return rootView;
     }
